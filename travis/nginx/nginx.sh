a043f8d6cb9c7db4470a84a8ed9fcb8746e540de
Jairo Moreno <master.zion@gmail.com>
2015-05-21 02:56:34 -0300

    nginx script

--- travis/nginx/nginx.sh
+++ travis/nginx/nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-sudo apt-get install nginx
-sudo cp -f ./travis/nginx/nginx.conf /etc/nginx/nginx.conf
-sudo cp -f ./travis/nginx/travis.vhost /etc/nginx/sites-available/default
+apt-get install nginx
+cp -f ./travis/nginx/nginx.conf /etc/nginx/nginx.conf
+cp -f ./travis/nginx/travis.vhost /etc/nginx/sites-available/default
 LOCALDIR="$(pwd)/public"
-sudo sed -i "s#LOCAL_DIR#${LOCALDIR}#" /etc/nginx/sites-available/default
+sed -i "s#LOCAL_DIR#${LOCALDIR}#" /etc/nginx/sites-available/default
   # php5 fpm
 cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-sudo service nginx restart
+service nginx restart
