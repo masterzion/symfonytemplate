5b5c99ffb18deb501a73ebad6f927c97d21b98cc
Jairo Moreno <master.zion@gmail.com>
2015-05-24 22:49:29 -0300

    behat and Selenium (Under development)

--- travis/nginx/nginx.sh
+++ travis/nginx/nginx.sh
@@ -4,6 +4,7 @@ cp -f ./travis/nginx/nginx.conf /etc/nginx/nginx.conf
 cp -f ./travis/nginx/travis.vhost /etc/nginx/sites-available/default
 LOCALDIR="$(pwd)/public"
 sed -i "s#LOCAL_DIR#${LOCALDIR}#" /etc/nginx/sites-available/default
+cat /etc/nginx/sites-available/default
   # php5 fpm
 cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
