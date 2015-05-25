2b0f2b8500d6dfb4ba6cadac6c0b8bc092afa7c9
Jairo Moreno <master.zion@gmail.com>
2015-05-25 00:48:27 -0300

    travis script

--- travis/nginx/nginx.sh
+++ travis/nginx/nginx.sh
@@ -6,9 +6,8 @@ LOCALDIR="$(pwd)/public"
 sed -i "s#LOCAL_DIR#${LOCALDIR}#" /etc/nginx/sites-available/default
 cat /etc/nginx/sites-available/default
   # php5 fpm
-cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 chmod -R 777 $LOCALDIR/app/logs/
 chmod -R 777 $LOCALDIR/app/cache/
+
 
 service nginx restart
